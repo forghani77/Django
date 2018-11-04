eb7998a274aca7ed5ec5f6337b98d1083c4b14db
Mohammad Hosein <you@example.com>
2018-11-04 07:53:30 +0330

    initial commit

---
+++ web/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin 
+from .models import Course , Mark
+
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Mark)
---
+++ web/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Course(models.Model):
+	text = models.CharField(max_length=255)
+	date = models.DateTimeField()
+	amount = models.FloatField()
+	user = models.ForeignKey(User, on_delete=models.PROTECT)
+class Mark(models .Model):
+	text = models.CharField(max_length=255)
+	date = models.DateTimeField()
+	amount = models.FloatField()
+	user = models.ForeignKey(User, on_delete=models.PROTECT)
